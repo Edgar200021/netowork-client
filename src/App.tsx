712007291d6967bbe4a37ca599737c872b21d428
Edgar Asatryan <edgarartur2000@mail.ru>
2024-06-23 12:59:50 +0400

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
+export const App = () => {
   const [count, setCount] = useState(0)
 
   return (
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -31,5 +31,3 @@ function App() {
     </>
   )
 }
-
-export default App
